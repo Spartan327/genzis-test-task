00d6ceeee49287375480a7bf9c95f79e5ff7c0e1
Spartan327 <vbutovchenko@mail.ru>
2022-02-17 11:59:57 +0300

    backup

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#export DB_TYPE=postgresql
+#export DB_USER=postgres
+#export DB_PASSWORD=375079
+#export DB_NAME=genzis
+#export DB_HOST=localhost
+#export DB_PORT=5432
+export SECRET_KEY='awEawdae*efsergewr39052jem!sef_efsef,llps'
+#export DATABASE_URL=postgresql://postgres:375079@localhost:5432/genzis
+#export DEV_DATABASE_URL=postgresql://postgres:375079@localhost:5432/genzis
+export CONFIG_NAME=development
+export FLASK_APP=backend 
+export FLASK_ENV=development
+flask db init
+flask db migrate
+flask db upgrade
+flask run
